03eaf88843f031f5280421ca00c3a0230d123da7
José Ignacio Escribano <jiep12345@hotmail.com>
2018-02-17 18:06:44 +0100

    Added design #4

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,67 @@
 <template lang="pug">
-  section.section
-    div.container
-      h1.title Hello World
-      p.subtitle My first website with
-        strong Bulma
+section.hero.is-fullheight
+  div.hero-head
+    header.navbar
+      div.container
+        div.navbar-brand
+          a.navbar-item
+            h1 Brand
+        div.navbar-burger.burger
+          span
+          span
+          span
+        div.navbar-menu
+          a.navbar-item.is-active Home
+          a.navbar-item Examples
+          a.navbar-item Documentation
+          span.navbar-item
+            a.button.is-warning.is-inverted
+              span Download
+
+  div.hero-body
+    div.container.has-text-centered
+      div.columns
+        div.column.is-10.is-offset-1
+          div.columns
+            div.column.is-info
+              figure.image.is3by4
+                img(:src="returnImage(paintings[0])")
+            div.column.is-info
+              figure.image.is3by4
+                img(:src="returnImage(paintings[1])")
+            div.column.is-info
+              figure.image.is3by4
+                img(:src="returnImage(paintings[2])")
+          div.columns
+            div.column.is-info
+              figure.image.is3by4
+                img(:src="returnImage(paintings[3])")
+            div.column.is-info
+              figure.image.is3by4
+                img(:src="returnImage(paintings[4])")
+            div.column.is-info
+              figure.image.is3by4
+                img(:src="returnImage(paintings[5])")
 </template>
 
 <script>
-// import axios from '~/plugins/axios'
+import axios from '~/plugins/axios'
+
+const BASE_IMAGES = '/images/'
 
 export default {
-  // async asyncData () {
-  //   let { data } = await axios.get('/api/paintings')
-  //   return { users: data }
-  // },
+  async asyncData () {
+    let { data } = await axios.get('/api/paintings')
+    return { paintings: data }
+  },
   head () {
     return {
       title: 'Paintings'
+    }
+  },
+  methods: {
+    returnImage (painting) {
+      return `${BASE_IMAGES}/${painting.img}`
     }
   }
 }
