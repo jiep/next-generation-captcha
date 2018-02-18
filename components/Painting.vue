d9108b5a1b16b72a37b1ade694c478aafb1a13a3
José Ignacio Escribano <jiep12345@hotmail.com>
2018-02-18 09:35:44 +0100

    Added fade effect. #4

--- components/Painting.vue
+++ components/Painting.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
-  div.column.is-info(id="container")
+  div.column(id="container")
     figure.image.is3by4
       img.img(:src="returnImage(painting)")
-      div.info
-        a.button.is-primary.is-outlined.is-inverted Real
-        a.button.is-primary.is-outlined.is-inverted Generated
+      div.overlay
+        div.field.is-grouped
+          p.control
+            a.button.is-primary.is-outlined.is-inverted Real
+          p.control
+            a.button.is-primary.is-outlined.is-inverted Generated
+        //- a.button.is-primary.is-outlined.is-inverted Real
+        //- a.button.is-primary.is-outlined.is-inverted Generated
 </template>
 
 <script>
-  const BASE_IMAGES = '/images/'
+  const BASE_IMAGES = '/images'
 
   export default {
     props: {
@@ -28,32 +33,28 @@
 <style scoped>
   #container {
     position: relative;
-    width: 50%;
-  }
-
-  .img {
-    opacity: 1;
-    display: block;
-    width: 100%;
+    width: auto;
     height: auto;
-    transition: background .5s ease;
   }
 
-  .info {
-    transition: .5s ease;
-    opacity: 0;
+  .overlay {
     position: absolute;
-    bottom: 5%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
+    left: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.4);
+    width: 100%;
+    height: 100%;
+    transition: 0.5s ease;
+    opacity: 0;
+    text-align: center;
+    padding-top: 90px;
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center;
   }
 
-  #container:hover .img {
-    opacity: 0.3;
-  }
-
-  #container:hover .info {
+  #container:hover .overlay {
     opacity: 1;
   }
 </style>
